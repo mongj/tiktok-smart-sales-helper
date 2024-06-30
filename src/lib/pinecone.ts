746236cb97082e97c259d03b11e67d8ea6a53d4d
positivelyjon <positivelyjon@gmail.com>
2024-06-30 09:28:50 +0900

    feat: create embedding logica and file upload area

---
+++ src/lib/pinecone.ts
@@ -0,0 +1,89 @@
+import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
+import { downloadFromS3 } from "./s3-server";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import {
+  Document,
+  RecursiveCharacterTextSplitter,
+} from "@pinecone-database/doc-splitter";
+import { getEmbeddings } from "./embeddings";
+import md5 from "md5";
+import { convertToASCII } from "./utils";
+
+export const getPineconeClient = () => {
+  return new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY!,
+  });
+};
+
+type PDFPage = {
+  pageContent: string;
+  metadata: {
+    loc: { pageNumber: number };
+  };
+};
+
+export async function loadS3IntoPinecone(fileKey: string) {
+  console.log("Downloading S3 into file system");
+  const file_name = await downloadFromS3(fileKey);
+  if (!file_name) {
+    throw new Error("Could not download file from s3");
+  }
+
+  console.log("loading pdf into memory" + file_name);
+  const loader = new PDFLoader(file_name);
+  const pages = (await loader.load()) as PDFPage[];
+
+  const documents = await Promise.all(
+    pages.map((page) => prepareDocument(page))
+  );
+
+  const vectors = await Promise.all(documents.flat().map(embedDocument));
+  const client = await getPineconeClient();
+  const pineconeIndex = client.Index("rizz-up");
+  const namespace = pineconeIndex.namespace(convertToASCII(fileKey)); // if files are not all in asci characters, will break
+
+  console.log("inserting vectors into pinecone");
+  await namespace.upsert(vectors);
+
+  return documents[0];
+}
+
+async function embedDocument(doc: Document) {
+  try {
+    const embeddings = await getEmbeddings(doc.pageContent);
+    const hash = md5(doc.pageContent);
+    return {
+      id: hash,
+      values: embeddings,
+      metadata: {
+        text: doc.metadata.text,
+        pageNumber: doc.metadata.number,
+      },
+    } as PineconeRecord;
+  } catch (e) {
+    console.log("error embedding document", e);
+    throw e;
+  }
+}
+
+export const truncateStringByBytes = (str: string, bytes: number) => {
+  const enc = new TextEncoder();
+  return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
+};
+
+async function prepareDocument(page: PDFPage) {
+  let { pageContent, metadata } = page;
+  pageContent = pageContent.replace(/\n/g, "");
+  //split the docs
+  const splitter = new RecursiveCharacterTextSplitter();
+  const docs = await splitter.splitDocuments([
+    new Document({
+      pageContent,
+      metadata: {
+        pageNumber: metadata.loc.pageNumber,
+        text: truncateStringByBytes(pageContent, 36000),
+      },
+    }),
+  ]);
+  return docs;
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function convertToASCII(str: string) {
+  // remove any non ascii characters
+  const asciiString = str.replace(/[^\x00x-\x7F]/g, "");
+  return asciiString;
+}
